706322fe4a1809c2f2e907cd817986a23568217e
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-09-09 10:55:17 -0700

    rename database and everything

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Scorsteezy"
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Scoresteezy"
 );
 
 module.exports = mongoose.connection;
