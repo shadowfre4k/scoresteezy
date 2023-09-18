bba21a21815870832b6e445298ebf9a177b6d1d9
Neta2393 <benetheahardin@yahoo.com>
2023-09-18 13:54:57 -0700

    Added rating to saved pokemon and also inserted funtion in h2 of saved Pokemon to render the amount of Pokemon that are saved

--- client/src/pages/SavedPokemon.jsx
+++ client/src/pages/SavedPokemon.jsx
@@ -23,7 +23,7 @@ const SavedPokemon = () => {
     });
   };
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  // create function that accepts the Pokemon's mongo _id value as param and deletes the Pokemon from the database
   const handleDeletePokemon = async (pokemon) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -36,7 +36,7 @@ const SavedPokemon = () => {
         variables: { pokeId: pokemon.pokeId },
       });
       console.log(response);
-      // upon success, remove book's id from localStorage
+      // upon success, remove Pokemon's id from localStorage
       removePokemonId(pokemon.pokeId);
     } catch (err) {
       console.error(err);
@@ -47,7 +47,7 @@ const SavedPokemon = () => {
   if (loading) {
     return <h2>LOADING...</h2>;
   }
-
+//Return either you have no saved Pokemon or the amount of Pokemon that you have saved
   return (
     <>
       <div className="text-light bg-dark p-5">
@@ -59,9 +59,9 @@ const SavedPokemon = () => {
         <h2 className="pt-5">
           {userData?.savedPokemon?.length
             ? `Viewing ${userData.savedPokemon.length} saved ${
-                userData.savedPokemon.length === 1 ? "book" : "books"
+                userData.savedPokemon.length === 1 ? "Pokemon" : "Pokemons"
               }:`
-            : "You have no saved books!"}
+            : "You have no saved Pokemon!"} 
         </h2>
         <Row>
           {userData?.savedPokemon?.map((pokemon) => {
@@ -77,7 +77,6 @@ const SavedPokemon = () => {
                   ) : null}
                   <Card.Body>
                     <Card.Title>{pokemon.name}</Card.Title>
-                    <p className="small">Authors: {pokemon.name}</p>
                     <Card.Text>{pokemon.description}</Card.Text>
                     
                     {/* Include the Rating Input */}
@@ -98,7 +97,7 @@ const SavedPokemon = () => {
                       className="btn-block btn-danger"
                       onClick={() => handleDeletePokemon(pokemon)}
                     >
-                      Delete this Book!
+                      Delete this Pokemon!
                     </Button>
                   </Card.Body>
                 </Card>
@@ -112,3 +111,118 @@ const SavedPokemon = () => {
 };
 
 export default SavedPokemon;
+
+// import React, { useState } from "react";
+// import { Container, Card, Button, Row, Col } from "react-bootstrap";
+// import { useQuery, useMutation } from "@apollo/client";
+// import { QUERY_ME } from "../utils/queries";
+// import { REMOVE_POKEMON } from "../utils/mutations";
+// import Auth from "../utils/auth";
+// import { removePokemonId } from "../utils/localStorage";
+
+// const SavedPokemon = () => {
+//   const { loading, data } = useQuery(QUERY_ME);
+//   const [removePokemon] = useMutation(REMOVE_POKEMON);
+
+//   const userData = data?.me || {};
+
+//   // Create a state variable to store user ratings for saved Pokemon
+//   const [userRatings, setUserRatings] = useState({});
+
+//   // Function to handle user rating input
+//   const handleRatingChange = (pokeId, rating) => {
+//     setUserRatings({
+//       ...userRatings,
+//       [pokeId]: rating,
+//     });
+//   };
+
+//   // create function that accepts the book's mongo _id value as param and deletes the book from the database
+//   const handleDeletePokemon = async (pokemon) => {
+//     const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+//     if (!token) {
+//       return false;
+//     }
+
+//     try {
+//       const response = await removePokemon({
+//         variables: { pokeId: pokemon.pokeId },
+//       });
+//       console.log(response);
+//       // upon success, remove book's id from localStorage
+//       removePokemonId(pokemon.pokeId);
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+
+//   // if data isn't here yet, say so
+//   if (loading) {
+//     return <h2>LOADING...</h2>;
+//   }
+
+//   return (
+//     <>
+//       <div className="text-light bg-dark p-5">
+//         <Container>
+//           <h1>Viewing saved Pokemon!</h1>
+//         </Container>
+//       </div>
+//       <Container>
+//         <h2 className="pt-5">
+//           {userData?.savedPokemon?.length
+//             ? `Viewing ${userData.savedPokemon.length} saved ${
+//                 userData.savedPokemon.length === 1 ? "book" : "books"
+//               }:`
+//             : "You have no saved books!"}
+//         </h2>
+//         <Row>
+//           {userData?.savedPokemon?.map((pokemon) => {
+//             return (
+//               <Col key={pokemon.pokeId} md="4">
+//                 <Card border="dark">
+//                   {pokemon.image ? (
+//                     <Card.Img
+//                       src={pokemon.image}
+//                       alt={`The cover for ${pokemon.name}`}
+//                       variant="top"
+//                     />
+//                   ) : null}
+//                   <Card.Body>
+//                     <Card.Title>{pokemon.name}</Card.Title>
+//                     <p className="small">Authors: {pokemon.name}</p>
+//                     <Card.Text>{pokemon.description}</Card.Text>
+                    
+//                     {/* Include the Rating Input */}
+//                     <select
+//                       value={userRatings[pokemon.pokeId] || 0}
+//                       onChange={(e) =>
+//                         handleRatingChange(pokemon.pokeId, parseInt(e.target.value))
+//                       }
+//                     >
+//                       {Array.from({ length: 6 }, (_, i) => (
+//                         <option key={i} value={i}>
+//                           {i}
+//                         </option>
+//                       ))}
+//                     </select>
+                    
+//                     <Button
+//                       className="btn-block btn-danger"
+//                       onClick={() => handleDeletePokemon(pokemon)}
+//                     >
+//                       Delete this Book!
+//                     </Button>
+//                   </Card.Body>
+//                 </Card>
+//               </Col>
+//             );
+//           })}
+//         </Row>
+//       </Container>
+//     </>
+//   );
+// };
+
+// export default SavedPokemon;
